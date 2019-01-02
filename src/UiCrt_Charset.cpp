406a19fe873c0f50c4f2c4408ee0674460752b37
jtbattle <frustum@pobox.com>
2019-01-01 18:43:17 -0600

    Native charset mapping table bug
    This has been there forever and nobody has ever noticed which tells me nobody ever uses it.  The mapping table was displaying "H" as "G".

--- src/UiCrt_Charset.cpp
+++ src/UiCrt_Charset.cpp
@@ -2740,7 +2740,7 @@ wxString unicode_xlat_char[128] = {
     L"0", L"1", L"2",  L"3", L"4",  L"5", L"6", L"7",
     L"8", L"9", L":",  L";", L"<",  L"=", L">", L"?",
     L"@", L"A", L"B",  L"C", L"D",  L"E", L"F", L"G",
-    L"G", L"I", L"J",  L"K", L"L",  L"M", L"N", L"O",
+    L"H", L"I", L"J",  L"K", L"L",  L"M", L"N", L"O",
     L"P", L"Q", L"R",  L"S", L"T",  L"U", L"V", L"W",
     L"X", L"Y", L"Z",  L"[", L"\\", L"]",
 
